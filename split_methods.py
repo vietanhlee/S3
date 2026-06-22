511d9253fe7ea50132154b2bcb7ad3e53592bce0
viet anh le <levietanhtrump@gmail.com>
2026-06-22 13:07:03 +0700

    up

--- split_methods.py
+++ split_methods.py
@@ -689,12 +689,12 @@ def stratified_group_kfold_split(
 # ============================================================
 
 SPLIT_METHODS = {
-	"PP1_Mahalanobis_Fixed": mahalanobis_fixed_split,
-	"PP2_Mahalanobis_Iterative": mahalanobis_iterative_split,
-	"PP3_Group_Based": group_based_split,
-	"PP4_Hierarchical_Clustering": hierarchical_clustering_split,
-	# "PP5_Cosine_Graph": cosine_graph_split,
-	# "PP6_Stratified_Random": stratified_random_split,
-	# "PP7_Adversarial_Validation": adversarial_validation_split,
-	# "PP8_StratifiedGroupKFold": stratified_group_kfold_split,
+	# "PP1_Mahalanobis_Fixed": mahalanobis_fixed_split,
+	# "PP2_Mahalanobis_Iterative": mahalanobis_iterative_split,
+	# "PP3_Group_Based": group_based_split,
+	# "PP4_Hierarchical_Clustering": hierarchical_clustering_split,
+	"PP5_Cosine_Graph": cosine_graph_split,
+	"PP6_Stratified_Random": stratified_random_split,
+	"PP7_Adversarial_Validation": adversarial_validation_split,
+	"PP8_StratifiedGroupKFold": stratified_group_kfold_split,
 }
